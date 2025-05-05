20fd816172bca123555602f89a8c04d1a420e624
Ramisa <139029556+nramisa@users.noreply.github.com>
2025-05-06 01:26:26 +0600

    Update AuthContext.js

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,8 +1,28 @@
-import { createContext } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
-const AuthContext = createContext({
-  auth: { token: null, role: null },
-  setAuth: () => {}
-});
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState(() => {
+    const storedAuth = localStorage.getItem('auth');
+    return storedAuth 
+      ? JSON.parse(storedAuth) 
+      : { token: null, user: null, role: null };
+  });
+
+  useEffect(() => {
+    if (auth.token) {
+      localStorage.setItem('auth', JSON.stringify(auth));
+    } else {
+      localStorage.removeItem('auth');
+    }
+  }, [auth]);
+
+  return (
+    <AuthContext.Provider value={{ auth, setAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
