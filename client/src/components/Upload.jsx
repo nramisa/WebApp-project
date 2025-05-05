b3d46c72680634bcfcb2be6a577a8cf9b21e7aff
Ramisa <139029556+nramisa@users.noreply.github.com>
2025-05-06 01:25:29 +0600

    Update Upload.jsx

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { Card, Form, Button } from 'react-bootstrap';
-import { AuthContext } from './App';
+import { AuthContext } from '../context/AuthContext';
 
 const Upload = ({ onUpload, loading }) => {
   const [file, setFile] = React.useState(null);
