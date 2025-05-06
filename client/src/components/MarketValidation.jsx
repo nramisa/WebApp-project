5033f52a704eb83dc4c22f7c1868167cd4d97eb3
Ramisa <139029556+nramisa@users.noreply.github.com>
2025-05-07 04:58:57 +0600

    Update MarketValidation.jsx

--- client/src/components/MarketValidation.jsx
+++ client/src/components/MarketValidation.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { AuthContext } from '../context/AuthContext';
+import { AuthContext } from '../context/AuthContext.jsx';
 import axios from 'axios';
 import styles from '../styles/Analysis.module.css';
 
