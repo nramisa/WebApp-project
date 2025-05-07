53a5f05acf10314e7292cea864dc92e84f6ce00d
Ramisa <139029556+nramisa@users.noreply.github.com>
2025-05-07 15:08:56 +0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import Home from './components/Home';
-import Analysis from './components/Analysis';
-import MarketValidation from './components/MarketValidation';
-import InvestorQA from './components/InvestorQA';
-import Upload from './components/Upload';
-import Footer from './components/Footer';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Upload from "./components/Upload";
+import Analysis from "./components/Analysis";
+import MarketValidation from "./components/MarketValidation";
+import InvestorQA from "./components/InvestorQA";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -14,10 +13,10 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/upload" element={<Upload />} />
           <Route path="/analysis" element={<Analysis />} />
           <Route path="/market-validation" element={<MarketValidation />} />
           <Route path="/investor-qa" element={<InvestorQA />} />
-          <Route path="/upload" element={<Upload />} />
         </Routes>
         <Footer />
       </div>
@@ -26,3 +25,4 @@ function App() {
 }
 
 export default App;
+
