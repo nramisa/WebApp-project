0e649c39987b8876f266a7a773b77963f1db3ca3
Ramisa <139029556+nramisa@users.noreply.github.com>
2025-05-07 14:45:12 +0600

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -6,12 +6,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
