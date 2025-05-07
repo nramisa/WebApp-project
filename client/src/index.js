e69bd34e26d817d9aa9d014e3dd6f5dbfa6b4118
Ramisa <139029556+nramisa@users.noreply.github.com>
2025-05-07 17:29:13 +0600

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,47 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+import ReactDOM from 'react-dom/client';
+
+
+import './index.css';
+
+
+import App from './App';
+
+
+import reportWebVitals from './reportWebVitals';
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
+
+
   <React.StrictMode>
+
+
     <App />
+
+
   </React.StrictMode>
+
+
 );
 
-reportWebVitals();
 
+
+
+
+reportWebVitals();
