5b33b40858c9dea9b6c179c9b7f7abc65e6f1b32
Ramisa <139029556+nramisa@users.noreply.github.com>
2025-05-07 19:04:32 +0600

    Create PublicNavbar.jsx

---
+++ client/src/components/PublicNavbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const PublicNavbar = () => (
+  <Navbar bg="white" expand="lg" className="shadow-sm">
+    <Container>
+      <Navbar.Brand as={Link} to="/" className="text-danger fw-bold">
+        PitchIn
+      </Navbar.Brand>
+      <Nav className="ms-auto">
+        <Nav.Link as={Link} to="/signup" className="mx-2">Sign Up</Nav.Link>
+        <Nav.Link as={Link} to="/login" className="mx-2">Login</Nav.Link>
+      </Nav>
+    </Container>
+  </Navbar>
+);
+
+export default PublicNavbar;
