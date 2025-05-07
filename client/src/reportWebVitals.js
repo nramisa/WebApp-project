f3c35f9b67ac11d31bd3e4012e0d07f7613e3cb6
Ramisa <139029556+nramisa@users.noreply.github.com>
2025-05-07 14:45:58 +0600

    Update reportWebVitals.js

--- client/src/reportWebVitals.js
+++ client/src/reportWebVitals.js
@@ -1,12 +1,13 @@
+import { getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
+
+// Pass a function to log results (or send to analytics)
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    getCLS(onPerfEntry);
+    getFID(onPerfEntry);
+    getFCP(onPerfEntry);
+    getLCP(onPerfEntry);
+    getTTFB(onPerfEntry);
   }
 };
 
