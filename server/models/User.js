f715f2520c4aa2bd5efcbb2332940173e21c7d5e
Ramisa <139029556+nramisa@users.noreply.github.com>
2025-05-09 22:16:04 +0600

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,13 @@
-// server/models/User.js
 const mongoose = require('mongoose');
+const crypto   = require('crypto');
 
 const userSchema = new mongoose.Schema({
-  name:      { type: String, required: true },
-  email:     { type: String, unique: true, required: true },
-  passwordHash: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
+  name:              { type: String, required: true },
+  email:             { type: String, unique: true, required: true },
+  passwordHash:      { type: String, required: true },
+  isVerified:        { type: Boolean, default: false },
+  verificationToken: { type: String, default: () => crypto.randomBytes(32).toString('hex') },
+  createdAt:         { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('User', userSchema);
